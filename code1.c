d626f604ab895fe6530a57371bec49fbdff5adc6
Yuta_Domen <yutamarodd0730@gmail.com>
2023-06-07 13:34:51 +0900

    テキストファイルから1バイトの文字のみを抽出

--- code1.c
+++ code1.c
@@ -8,25 +8,18 @@ int main(void) {
         int i = 0;
         char moji[999999];
         fp = fopen(fname, "r");
-
         for(i = 0; ((moji[i] = fgetc(fp)) != EOF); i++) {
-
                 if(isalpha(moji[i])){
                         putchar(moji[i]);
                 }
-
                 else if(moji[i] == 10){
                 moji[i] = 10;
                 putchar(moji[i]);
                 }
-
                 else{
                         moji[i] = 32;
                         putchar(moji[i]);
                 }
-
         }
-
 return 0;
-
 }
